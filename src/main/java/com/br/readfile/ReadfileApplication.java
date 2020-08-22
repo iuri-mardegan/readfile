1826f4086c3ae18bc663c5c8bcd81cc01d397582
Iuri <iurimardegan@gmail.com>
2020-08-22 19:14:08 -0300

    Init

--- src/main/java/com/br/readfile/ReadfileApplication.java
+++ src/main/java/com/br/readfile/ReadfileApplication.java
@@ -3,11 +3,20 @@ package com.br.readfile;
 import com.br.readfile.modelo.leitura.ModeloDefault;
 import com.br.readfile.service.ProcessFile;
 
+import static java.lang.Thread.sleep;
+
 public class ReadfileApplication {
 
     public static void main(String[] args) {
-        ProcessFile processFile = new ProcessFile();
-        processFile.read(new ModeloDefault());
+        while (true) {
+            ProcessFile processFile = new ProcessFile();
+            processFile.read(new ModeloDefault());
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
--- src/main/java/com/br/readfile/service/ProcessFile.java
+++ src/main/java/com/br/readfile/service/ProcessFile.java
@@ -13,6 +13,7 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class ProcessFile {
 
@@ -26,16 +27,16 @@ public class ProcessFile {
     private List<Cliente> clienteList;
     private List<Venda> vendaList;
 
-    public void read(ModeloAbstract modelo) {
-        File path = new File(PATH_IN);
-        Arrays.stream(Objects.requireNonNull(path.list()))
-                .forEach(f -> processFile(modelo, f));
-    }
-
     private void inicializa() {
         vendedorList = new ArrayList<>();
         clienteList = new ArrayList<>();
         vendaList = new ArrayList<>();
+    }
+
+    public void read(ModeloAbstract modelo) {
+        File path = new File(PATH_IN);
+        Arrays.stream(Objects.requireNonNull(path.list()))
+                .forEach(f -> processFile(modelo, f));
     }
 
     private void processFile(ModeloAbstract modelo, String fileName) {
@@ -49,6 +50,7 @@ public class ProcessFile {
                 processData(data.split(modelo.getSeparador()));
             }
             myReader.close();
+            file.delete();
             writeFile(fileName);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -72,7 +74,7 @@ public class ProcessFile {
     }
 
     private List<Item> generateItemList(String itemListString) {
-        List<Item> itemList = new ArrayList<Item>();
+        List<Item> itemList = new ArrayList<>();
         try {
             JSONArray jsonArray = new JSONArray(itemListString);
             for (int i = 0; i < jsonArray.length(); i++) {
@@ -97,7 +99,7 @@ public class ProcessFile {
             myWriter.write("Clientes importados: "+clienteList.size());
             myWriter.write("\nVendedores importados: "+vendedorList.size());
             myWriter.write("\nId maior venda: "+maiorVenda());
-            myWriter.write("\nPior Vendedor: ");
+            myWriter.write("\nPior Vendedor: "+piorVendedor());
             myWriter.close();
         } catch (IOException e) {
             e.printStackTrace();
@@ -107,5 +109,29 @@ public class ProcessFile {
     private Integer maiorVenda(){
         Collections.sort(vendaList);
         return vendaList.get(vendaList.size() - 1).getSaleId();
+    }
+
+    private String piorVendedor(){
+        Map<String, Double> vendasMap = new HashMap<>();
+        vendaList.forEach(v ->{
+            if(vendasMap.containsKey(v.getSalesmanName())){
+                vendasMap.put(v.getSalesmanName(),
+                        vendasMap.get(v.getSalesmanName()) +
+                        v.getItemList().stream().map(Item::getPrice)
+                                .reduce(0.0, Double::sum));
+            }else{
+                vendasMap.put(v.getSalesmanName(),
+                        v.getItemList().stream().map(Item::getPrice)
+                                .reduce(0.0, Double::sum));
+            }
+        });
+        vendasMap.entrySet()
+                .stream()
+                .sorted((Map.Entry.<String, Double>comparingByValue().reversed()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+        return vendasMap.entrySet()
+                .stream()
+                .min(Comparator.comparing(Map.Entry::getValue))
+                .get().getKey();
     }
 }
